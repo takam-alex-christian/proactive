d44f7f73b15ca82ef5a18f81a59a781838630dc0
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-03 10:45:36 +0800

    TodoManager Feature test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,24 @@
 "use client";
 import ToDoItem from "@/components/ToDoItem";
+import ToDoManager from "@/features/ToDoManager";
+import ToDoItemsLayout from "@/layouts/ToDoItemsLayout";
 
 export default function () {
-  return <ToDoItem />;
+  return (
+    <main className="flex h-screen w-screen ">
+      {/* <ToDoItem content="build a project every week" /> */}
+
+      {/* <ToDoItemsLayout>
+        <ToDoItem content="Build multiple projects" />
+        <ToDoItem content="Build portfolios projects" />
+      </ToDoItemsLayout> */}
+
+      <ToDoManager>
+        <ToDoItemsLayout>
+          <ToDoItem content="Build multiple projects" />
+          <ToDoItem content="Build portfolios projects" />
+        </ToDoItemsLayout>
+      </ToDoManager>
+    </main>
+  );
 }
