0a5c6d4cbb6c329432ffd0d1918fc556362c9987
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-04 00:49:24 +0800

    fixing font and transiting to Inter from google fonts instead of Geist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 import * as React from "react";
 
 import ProactiveApp from "./_app";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const interFont = Inter({});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${interFont.className} antialiased`}>
         <ProactiveApp>{children}</ProactiveApp>
       </body>
     </html>
