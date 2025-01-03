55b5b86d7f2258b08c8510b8a1d18a767218017b
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-04 00:50:57 +0800

    base editor component finalised

--- src/components/ToDoEditor.tsx
+++ src/components/ToDoEditor.tsx
@@ -14,11 +14,12 @@ export default function ToDoEditor() {
   return (
     <div className="">
       <div className="flex flex-row justify-between items-center h-10">
-        <div className="flex flex-row items-center gap-2 px-3 bg-black text-white w-full h-full cursor-text focus-within:bg-slate-800 rounded-full">
-          <PlusSignIcon className="text-white w-5 h-5 font-extrabold " />
+        <div className="flex flex-row items-center gap-2 px-3 bg-black text-white w-full h-full cursor-text focus-within:bg-black rounded-full">
+          <PlusSignIcon className=" text-white w-2 h-5 font-extrabold " />
           <input
-            className="w-full h-full focus:outline-none bg-transparent"
+            className="w-full h-full font-medium focus:outline-none bg-transparent"
             placeholder="What's up ?"
+            name="todo_editor"
           />
         </div>
       </div>
