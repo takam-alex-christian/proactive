dfa038416900ac6d2709fc93764e8b626e8f4cb9
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-03 10:45:13 +0800

    ui improvement

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -15,7 +15,7 @@ export default function ({ content }: { content: string }) {
   const [isChecked, setCheckState] = useState(false);
 
   return (
-    <div className="flex flex-row justify-between w-full items-center h-9 px-2 py-6 rounded-md bg-white">
+    <div className="flex flex-row justify-between w-full items-center h-9 px-2 py-6 rounded-md bg-white shadow-sm ">
       <Checkbox isSelected={isChecked} onValueChange={setCheckState} size="lg">
         {content}
       </Checkbox>
