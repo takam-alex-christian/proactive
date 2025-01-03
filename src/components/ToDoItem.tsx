5cc953a2f7c1cab32e5e15d1d02b6c86ac2df3f3
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-03 10:14:13 +0800

    implementing todo item drop down menu and content props

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
-import { Checkbox } from "@nextui-org/react";
 
-export default function () {
+import { Checkbox, Button } from "@nextui-org/react";
+
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+} from "@nextui-org/dropdown";
+
+export default function ({ content }: { content: string }) {
   const [isChecked, setCheckState] = useState(false);
 
   return (
-    <div className="flex flex-row items-center h-9 px-2">
+    <div className="flex flex-row w-full items-center h-9 px-2 rounded-md bg-white">
       <Checkbox isSelected={isChecked} onValueChange={setCheckState}>
-        {}
+        {content}
       </Checkbox>
+
+      {/* options */}
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="flat" size="sm">
+            Open Menu
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          {/* <DropdownItem key="new">New file</DropdownItem> */}
+          <DropdownItem key="delete" className="text-danger" color="danger">
+            Delete
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 }
