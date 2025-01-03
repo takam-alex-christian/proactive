2cb06d291f40d61c146c63aa6ab0642c1c14cb6b
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-04 00:50:37 +0800

    Bugs fixed on plusSignIcon path element. and size ajusted for visual consistency

--- src/assets/uicons.tsx
+++ src/assets/uicons.tsx
@@ -104,8 +104,8 @@ const MoreVerticalIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 const PlusSignIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width="24"
-    height="24"
+    width="20"
+    height="20"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -113,9 +113,9 @@ const PlusSignIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <path
       d="M12 5V19M5 12H19"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
