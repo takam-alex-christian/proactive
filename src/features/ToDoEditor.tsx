da3e1e7447e13db5648ebc5eee506354c071ff99
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-04 21:31:24 +0800

    ToDoEditor feature updated to affect todocontext state

--- src/features/ToDoEditor.tsx
+++ src/features/ToDoEditor.tsx
@@ -1,8 +1,13 @@
-import { FormEvent, useState, ChangeEvent } from "react";
+"use client";
+import { FormEvent, useState, ChangeEvent, useContext } from "react";
+import { ToDoDispatcher } from "@/lib/contexts";
+
 import ToDoEditor from "@/components/ToDoEditor";
 
 export default function () {
   const [editorValue, setEditorValue] = useState("");
+
+  const toDoDispatch = useContext(ToDoDispatcher);
 
   function editorValueChangeHandler(e: ChangeEvent<HTMLInputElement>) {
     setEditorValue(e.currentTarget.value);
@@ -10,6 +15,11 @@ export default function () {
 
   function onSubmit(e: FormEvent) {
     e.preventDefault();
+
+    toDoDispatch({
+      type: "created",
+      payload: { id: "0", text: editorValue, completed: false },
+    });
   }
   return (
     <ToDoEditor
