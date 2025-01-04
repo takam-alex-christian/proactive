63647db0d97e6c52d4e0667f76d0ff652ae6a8fd
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-04 21:33:00 +0800

    using context in app and adding editor and manager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useContext, useEffect, useReducer } from "react";
+import { ToDoContext, ToDoDispatcher, initialToDoState } from "@/lib/contexts";
+import toDoReducer from "@/lib/reducers";
+
+import ToDoEditor from "@/features/ToDoEditor";
+import ToDoManager from "@/features/ToDoManager";
+
 export default function Home() {
-  return <div>hello world!</div>;
+  const [todoState, todoDispatch] = useReducer(toDoReducer, initialToDoState);
+
+  return (
+    <main>
+      <ToDoContext.Provider value={todoState}>
+        <ToDoDispatcher.Provider value={todoDispatch}>
+          {/* todo header */}
+
+          {/* todo manager */}
+          <ToDoManager />
+
+          <ToDoEditor />
+        </ToDoDispatcher.Provider>
+      </ToDoContext.Provider>
+    </main>
+  );
 }
