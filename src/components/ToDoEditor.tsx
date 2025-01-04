3c8eb33a27cb7137b6bd9677d9f79c53a766c5a1
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-04 18:13:58 +0800

    updating ToDoEditor component to hand control to the TodoEditor feature which handles functionality and states

--- src/components/ToDoEditor.tsx
+++ src/components/ToDoEditor.tsx
@@ -1,3 +1,5 @@
+import { useState, ChangeEvent } from "react";
+
 import { Textarea, extendVariants } from "@nextui-org/react";
 import { PlusSignIcon } from "@/assets/uicons";
 
@@ -10,19 +12,32 @@ const CustomTextArea = extendVariants(Textarea, {
     },
   },
 });
-export default function ToDoEditor() {
+export default function ToDoEditor({
+  editorValue,
+  onSubmit,
+  onChange,
+}: {
+  editorValue: string;
+  onSubmit: React.FormEventHandler;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}) {
   return (
     <div className="">
-      <div className="flex flex-row justify-between items-center h-10">
-        <div className="flex flex-row items-center gap-2 px-3 bg-black text-white w-full h-full cursor-text focus-within:bg-black rounded-full">
-          <PlusSignIcon className=" text-white w-2 h-5 font-extrabold " />
-          <input
-            className="w-full h-full font-medium focus:outline-none bg-transparent"
-            placeholder="What's up ?"
-            name="todo_editor"
-          />
+      <form onSubmit={onSubmit}>
+        <div className="flex flex-row justify-between items-center h-10">
+          <div className="flex flex-row items-center gap-2 px-3 bg-black text-white w-full h-full cursor-text focus-within:bg-black rounded-full">
+            <PlusSignIcon className=" text-white w-2 h-5 font-extrabold " />
+            <input
+              value={editorValue}
+              onChange={onChange}
+              autoComplete="off"
+              className="w-full h-full font-medium focus:outline-none bg-transparent"
+              placeholder="What's up ?"
+              name="todo_editor"
+            />
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
