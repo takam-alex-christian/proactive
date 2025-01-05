d5974bc93d7c74c2d7d5f5b5b5b02b283d02bc72
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-05 17:45:12 +0800

    AppHeaderFeature temporary implementation

---
+++ src/features/AppHeaderFeature.tsx
@@ -0,0 +1,6 @@
+"use client";
+import AppHeaderCompoment from "@/components/AppHeaderCompoment";
+
+export default function () {
+  return <AppHeaderCompoment username="Chris" date={Date.now()} />;
+}
