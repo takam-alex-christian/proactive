31e1759e5c8e040340c29e59d277eced6d5fdfd1
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-05 18:41:15 +0800

    AppContainer Layout used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,26 @@ import ToDoEditor from "@/features/ToDoEditor";
 import ToDoManager from "@/features/ToDoManager";
 import AppHeaderFeature from "@/features/AppHeaderFeature";
 import AppContainer from "@/layouts/AppContainer";
+import ToDoEditorLayout from "@/layouts/ToDoEditorLayout";
 
 export default function Home() {
   const [todoState, todoDispatch] = useReducer(toDoReducer, initialToDoState);
 
   return (
-    <AppContainer>
-      <ToDoContext.Provider value={todoState}>
-        <ToDoDispatcher.Provider value={todoDispatch}>
+    <ToDoContext.Provider value={todoState}>
+      <ToDoDispatcher.Provider value={todoDispatch}>
+        <AppContainer>
           {/* todo header */}
           <AppHeaderFeature />
 
           {/* todo manager */}
           <ToDoManager />
 
-          <ToDoEditor />
-        </ToDoDispatcher.Provider>
-      </ToDoContext.Provider>
-    </AppContainer>
+          <ToDoEditorLayout>
+            <ToDoEditor />
+          </ToDoEditorLayout>
+        </AppContainer>
+      </ToDoDispatcher.Provider>
+    </ToDoContext.Provider>
   );
 }
