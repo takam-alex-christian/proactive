67bbca2278341e34a799c8a57141155607e3c32d
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-09 11:52:24 +0800

    ui update

--- src/layouts/ToDoItemsLayout.tsx
+++ src/layouts/ToDoItemsLayout.tsx
@@ -1,3 +1,3 @@
 export default function ({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col w-full gap-2 ">{children}</div>;
+  return <div className="relative flex flex-col w-full gap-2 ">{children}</div>;
 }
