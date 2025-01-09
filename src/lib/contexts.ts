c8d3621a415c4e8d774b1f3f6f7453f1e4a01902
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-09 10:16:09 +0800

    context update to reflect ToDoStateType changes

--- src/lib/contexts.ts
+++ src/lib/contexts.ts
@@ -1,7 +1,11 @@
 import { createContext, Dispatch } from "react";
 import { ToDoStateType, ToDoReducerActionType } from "./types";
 
-const initialToDoState: ToDoStateType = { todos: [] };
+const initialToDoState: ToDoStateType = {
+  selectedCollection: "",
+  collections: [],
+  todos: [],
+};
 
 const ToDoContext = createContext<ToDoStateType>(initialToDoState);
 const ToDoDispatcher = createContext<Dispatch<ToDoReducerActionType>>(() => {});
