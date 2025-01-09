db28072fc6e5d69648ca3ed04c83b835d390ce34
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-09 14:33:14 +0800

    updated to incorporate presence animations and a placeholder for and empty collection list in state

--- src/features/CollectionManager.tsx
+++ src/features/CollectionManager.tsx
@@ -1,33 +1,54 @@
 import { useContext } from "react";
 import { ToDoContext, ToDoDispatcher } from "@/lib/contexts";
 import { Button } from "@nextui-org/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function () {
   const toDoState = useContext(ToDoContext);
   const toDoDispatch = useContext(ToDoDispatcher);
 
   return (
-    <div className="flex flex-col min-h-10">
-      {toDoState.collections.map((eachCollection) => {
-        return (
-          <Button
-            onPress={() => {
-              toDoDispatch({
-                type: "selected_collection_changed",
-                payload: { name: eachCollection.name },
-              });
-            }}
-            key={eachCollection.id}
-            className={`flex items-center justify-start ${
-              toDoState.selectedCollection == eachCollection.name
-                ? "bg-[#F6F6F6]"
-                : "bg-transparent "
-            }`}
+    <div className="relative flex flex-col min-h-10">
+      <AnimatePresence>
+        {toDoState.collections.length == 0 && (
+          <motion.div
+            className="absolute text-gray-700"
+            initial={{ scale: 0, opacity: 0, left: -500 }}
+            animate={{ scale: 1, opacity: 1, left: 0 }}
+            exit={{ scale: 0, opacity: 0, left: -500 }}
           >
-            {eachCollection.name}
-          </Button>
-        );
-      })}
+            <h3 className="font-semibold text-base">No Collections!</h3>
+            <p className="font-medium text-sm">create a collection first</p>
+          </motion.div>
+        )}
+        {toDoState.collections.map((eachCollection) => {
+          return (
+            <motion.div
+              key={eachCollection.id}
+              className="flex flex-row w-full"
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+            >
+              <Button
+                onPress={() => {
+                  toDoDispatch({
+                    type: "selected_collection_changed",
+                    payload: { name: eachCollection.name },
+                  });
+                }}
+                key={eachCollection.id}
+                className={`flex items-center justify-start w-full ${
+                  toDoState.selectedCollection == eachCollection.name
+                    ? "bg-[#F6F6F6]"
+                    : "bg-transparent "
+                }`}
+              >
+                {eachCollection.name}
+              </Button>
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 }
