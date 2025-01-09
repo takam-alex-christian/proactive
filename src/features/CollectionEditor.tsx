30b79f3a8bfceae6021c76b4e2357edad7a36a3d
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-09 13:50:08 +0800

    collection editor: double collection name forbid, editorState implmented to replace editor value

--- src/features/CollectionEditor.tsx
+++ src/features/CollectionEditor.tsx
@@ -5,31 +5,52 @@ import { ToDoContext, ToDoDispatcher } from "@/lib/contexts";
 import SimpleEditor from "@/components/SimpleEditor";
 
 export default function () {
-  const [editorValue, setEditorValue] = useState("");
+  const [editorState, setEditorState] = useState<{
+    text: string;
+    isError: boolean;
+    errorMessage: string;
+  }>({
+    text: "",
+    isError: false,
+    errorMessage: "",
+  });
 
   const toDoDispatch = useContext(ToDoDispatcher);
   const toDoState = useContext(ToDoContext);
 
   function editorValueChangeHandler(e: ChangeEvent<HTMLInputElement>) {
-    setEditorValue(e.currentTarget.value);
+    let textValue: string = e.currentTarget.value;
+    setEditorState((prevState) => {
+      return { ...prevState, text: textValue };
+    });
   }
 
   function onSubmit(e: FormEvent) {
     e.preventDefault();
 
+    if (
+      toDoState.collections.find((collectionName) => {
+        return collectionName.name == editorState.text;
+      })
+    ) {
+      // raise visual indicator
+      return;
+    }
     toDoDispatch({
       type: "collection_created",
       payload: {
-        name: editorValue,
+        name: editorState.text,
         id: Date.now().toString(),
       },
     });
 
-    setEditorValue("");
+    setEditorState((prevState) => {
+      return { ...prevState, text: "", isError: false, errorMessage: "" };
+    });
   }
   return (
     <SimpleEditor
-      editorValue={editorValue}
+      editorState={editorState}
       onChange={editorValueChangeHandler}
       onSubmit={onSubmit}
       placeholder="create new collection"
